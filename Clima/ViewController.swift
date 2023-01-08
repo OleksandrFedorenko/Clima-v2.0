ad139a43fbe83a51044b6b97b44edf39a116fa0d
Summer <afedorenko02@gmail.com>
2023-01-08 15:52:33 +0200

    Initial Commit

---
+++ Clima/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Clima
+//
+//  Created by Олександр Федоренко on 08.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
