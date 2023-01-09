d4a8a8723c73f6c6176d1c888bfe16fd0276d2c7
Summer <afedorenko02@gmail.com>
2023-01-09 13:09:43 +0200

    add protocol UITextFiedDelegate

--- Clima/ViewController.swift
+++ Clima/ViewController.swift
@@ -7,11 +7,44 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
+
+    @IBOutlet weak var searchTextView: UITextField!
+    
+
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        searchTextView.delegate = self
+        
         // Do any additional setup after loading the view.
+    }
+
+    
+    @IBAction func searchPressed(_ sender: UIButton) {
+        print(searchTextView.text!)
+        searchTextView.endEditing(true)
+    }
+
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        print(searchTextView.text!)
+        searchTextView.endEditing(true)
+        return true
+    }
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        if searchTextView.text != ""{
+            print(searchTextView.text!)
+            return true
+        }else{
+            searchTextView.placeholder  = "type something"
+            return false
+            
+        }
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField, reason: UITextField.DidEndEditingReason) {
+        searchTextView.text = ""
     }
 }
 
